4bcedf9c64d9a0cdf6ce1b275f183466b63cdcd1
Bernabe <bernabeferreyra78@gmail.com>
2026-07-14 15:18:33 -0300

    Add: implementación de IEntityConfiguration para Appointment, Availability, AvailabilitySlot, y Patient. Upd: implementación de Doctor y Speciality IEntityConfiguration

---
+++ Dsw2026Tpi.Data/Configurations/AvailabilityConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Data.Configuration
+{
+    public class AvailabilityConfiguration : IEntityTypeConfiguration<Availability>
+    {
+        public void Configure(EntityTypeBuilder<Availability> builder)
+        {
+            builder.ToTable("AvailabilityRules");
+            builder.HasIndex(a => new 
+            { 
+                a.DoctorId,
+                a.Year, 
+                a.Month,
+                a.DayOfWeek,
+                a.StartTime,
+                a.EndTime 
+            }).IsUnique();
+            builder.Property(a => a.Month).IsRequired();
+            builder.Property(a => a.Year).IsRequired();
+            builder.Property(a => a.DayOfWeek).IsRequired();
+            builder.Property(a => a.StartTime).IsRequired();
+            builder.Property(a => a.EndTime).IsRequired();
+        }
+    }
+}
---
+++ Dsw2026Tpi.Data/Configurations/AvailabilitySlotConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Data.Configuration
+{
+    public class AvailabilitySlotConfiguration : IEntityTypeConfiguration<AvailabilitySlot>
+    {
+        public void Configure(EntityTypeBuilder<AvailabilitySlot> builder)
+        {
+            builder.ToTable("AvailabilitySlots");
+            builder.HasIndex(a => new
+            {
+                // a.Availability.DoctorId,
+                a.Date,
+                a.StartTime
+            });
+            builder.Property(a => a.Date).IsRequired();
+            builder.Property(a => a.StartTime).IsRequired();
+            builder.Property(a => a.EndTime).IsRequired();
+            builder.Property(a => a.Status).HasMaxLength(20);
+        }
+    }
+}
