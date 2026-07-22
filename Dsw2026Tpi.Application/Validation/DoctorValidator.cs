3bb5e405c0deff36e4450a3e3016938ecfb420f4
MarianoRuiz <marianoruizcoronel@gmail.com>
2026-07-22 17:15:07 -0300

    Refactor: Adaptacion de ExceptionHandlingMiddleware, retorno de excepciones mejorado perteneciente a SpecilityValidator

--- Dsw2026Tpi.Application/Validation/DoctorValidator.cs
+++ Dsw2026Tpi.Application/Validation/DoctorValidator.cs
@@ -1,30 +1,29 @@
-﻿using Dsw2026Tpi.Application.Dtos;
-using Dsw2026Tpi.CrossCutting.Exceptions;
-using Dsw2026Tpi.CrossCutting.Resources;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using Dsw2026Tpi.Application.Dtos;
+//using Dsw2026Tpi.CrossCutting.Exceptions;
+//using Dsw2026Tpi.CrossCutting.Resources;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Dsw2026Tpi.Application.Validation
-{
-    public class DoctorValidator
-    {
-        public static void Validate(DoctorModel.Request request)
-        {
-            if (string.IsNullOrWhiteSpace(request.Name))
-            {
-                throw new ValidationException("El nombre no puede ser nulo o vacío.", ErrorCodes.VALIDATION_ERROR);
-            }
-            
-            if (request.SpecialityId == Guid.Empty)
-            {
-                throw new ValidationException("El ID de la especialidad no puede estar vacío.", ErrorCodes.VALIDATION_ERROR);
-            }
+//namespace Dsw2026Tpi.Application.Validation;
 
-            if(request.Name.Length > 100 || request.Name.Length <3)
-            {
-                throw new ValidationException("El nombre no puede tener más de 100 caracteres.", ErrorCodes.VALIDATION_ERROR);
-            }
-        }
-    }
-}
+//public class DoctorValidator
+//{
+//    public static void Validate(DoctorModel.Request request)
+//    {
+//        if (string.IsNullOrWhiteSpace(request.Name))
+//        {
+//            throw new ValidationException("El nombre no puede ser nulo o vacío.", ErrorCodes.VALIDATION_ERROR);
+//        }
+        
+//        if (request.SpecialityId == Guid.Empty)
+//        {
+//            throw new ValidationException("El ID de la especialidad no puede estar vacío.", ErrorCodes.VALIDATION_ERROR);
+//        }
+
+//        if(request.Name.Length > 100 || request.Name.Length <3)
+//        {
+//            throw new ValidationException("El nombre no puede tener más de 100 caracteres.", ErrorCodes.VALIDATION_ERROR);
+//        }
+//    }
+//}
