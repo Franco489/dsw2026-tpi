e49044673117e538694e6cb42d2c5c2915cbb24a
Franco489 <francoreyes597gmail.com>
2026-08-02 20:18:47 -0300

    Upd: Se agregó el campo id al endpoint doctors/getAvailabilities

--- Dsw2026Tpi.Application/Dtos/AvailabilityModel.cs
+++ Dsw2026Tpi.Application/Dtos/AvailabilityModel.cs
@@ -8,5 +8,5 @@ public record AvailabilityModel
 {
     public record Request(Guid DoctorId, List<DaySchedule> Days);
     public record Response(Guid DoctorId, List<DaySchedule> Days);
-    public record DaySchedule(string Day,TimeOnly StartTime, TimeOnly EndTime);
+    public record DaySchedule(Guid id, string Day,TimeOnly StartTime, TimeOnly EndTime);
 }
--- Dsw2026Tpi.Application/Services/DoctorService.cs
+++ Dsw2026Tpi.Application/Services/DoctorService.cs
@@ -39,7 +39,7 @@ public class DoctorService : IDoctorService
 
         var slots = await _persistence.Paginate<AvailabilitySlot, DateOnly>(pageSize, pageIndex,
                                                                    s => s.Availability.DoctorId == id, s => s.Date);
-        return slots.Map(s => new AvailabilityModel.DaySchedule(s.Date.ToString("dd/MM/yyyy"), s.EndTime, s.StartTime));
+        return slots.Map(s => new AvailabilityModel.DaySchedule(s.Id, s.Date.ToString("dd/MM/yyyy"), s.StartTime, s.EndTime));
 
 
     }
