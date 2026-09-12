9aa312c25b845c0972228fd5c24230a5d0f09ee4
Franco489 <francoreyes597gmail.com>
2026-09-12 18:59:20 -0300

    Upd: Add migracion para cambio de nombre de Specialties

---
+++ Dsw2026Tpi.Data/Migrations/20260912215429_ChangeSpecialtiesName.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dsw2026Tpi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSpecialtiesName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Doctors_Specialities_SpecialityId",
+                table: "Doctors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Specialities",
+                table: "Specialities");
+
+            migrationBuilder.RenameTable(
+                name: "Specialities",
+                newName: "Specialties");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Specialities_Name",
+                table: "Specialties",
+                newName: "IX_Specialties_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Specialties",
+                table: "Specialties",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Doctors_Specialties_SpecialityId",
+                table: "Doctors",
+                column: "SpecialityId",
+                principalTable: "Specialties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Doctors_Specialties_SpecialityId",
+                table: "Doctors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Specialties",
+                table: "Specialties");
+
+            migrationBuilder.RenameTable(
+                name: "Specialties",
+                newName: "Specialities");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Specialties_Name",
+                table: "Specialities",
+                newName: "IX_Specialities_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Specialities",
+                table: "Specialities",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Doctors_Specialities_SpecialityId",
+                table: "Doctors",
+                column: "SpecialityId",
+                principalTable: "Specialities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
